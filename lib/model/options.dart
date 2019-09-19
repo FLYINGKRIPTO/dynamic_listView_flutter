2526e6891547b5a8b6b18a34b0a1bcf5e81a3ef2
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2019-09-19 09:52:36 +0530

    Initial commit

---
+++ lib/model/options.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_app_myapp/model/options_data.dart';
+class Options {
+   String _title;
+
+   List<String> _subOptions = [];
+
+   Options(this._title,this._subOptions);
+
+   List<String> get subOptions => _subOptions;
+
+   set subOptions(List<String> value) {
+     _subOptions = value;
+   }
+
+   String get title => _title;
+
+   set title(String value) {
+     _title = value;
+   }
+
+
+}
---
+++ lib/model/options_data.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+
+import 'options.dart';
+class OptionsData{
+  List<Options> _optionsList = [
+
+    Options("Account",["Change Email","Name","Change Password","Logout"]),
+    Options("Premium",["Available Plans","Due date","Current Plan","Invoices","Cancel/Update Subscription",
+    "Contact Customer Care"]),
+    Options("App",["Theme and Layout","Sync and Storage","Archive","Home/Inbox screen default",
+    "Details Screen customization","Folders and Tags"]),
+    Options("Referals",["Refer a friend"]),
+    Options("Import",['Import from Pocket','Import from Instapaer','Import from Browser']),
+    Options("Help and Feedback",['FAQ','Guide','Submit and Feedback','Contact customer care',
+    'Open Issues','Enable Analytics','Enable Crash Reporting','Enable Issue Logging']),
+    Options("Privacy Policy",['Privacy Policy','Terms and Condition']),
+    Options('About The App',['Version','About Us','Follow on twitter','Follow on Instapaper',
+    'Website','Open Source license'
+    ]),
+    Options("Logout",[]),
+
+  ];
+  int getOptionListLength(){
+  //  debugPrint( 'Length = $_optionsList.length');
+    int length = _optionsList.length;
+    debugPrint('Length = $length');
+    return _optionsList.length;
+  }
+
+  String getOption(int index){
+
+    return _optionsList[index].title;
+  }
+
+  List<String> getSubOptions(int index){
+    return _optionsList[index].subOptions;
+  }
+
+}
