1617e0b97cc152e1dbd50c3f0056d8b690458260
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2019-09-20 10:19:35 +0530

    Theme and Layout Widgets ready

--- lib/Screens/themeandLayout.dart
+++ lib/Screens/themeandLayout.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+
 class ThemeAndLayout extends StatefulWidget {
   String appBarTitle;
 
   ThemeAndLayout(this.appBarTitle);
+
   @override
   _ThemeAndLayoutState createState() => _ThemeAndLayoutState(this.appBarTitle);
 }
@@ -15,9 +17,80 @@ class _ThemeAndLayoutState extends State<ThemeAndLayout> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      title: 'Theme and Layout',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appBarTitle),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(child: getThemeContainers()),
+            Expanded(child: getFontSizeSlider()),
+            Expanded(child: getMarginsSetter()),
+            Expanded(child: getLineHeightSetter()),
+            Expanded(child: getFontSetter()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getThemeContainers() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.only(left: 20.5, right: 20.5),
+            child: RaisedButton(
+              color: Colors.white70,
+              elevation: 10.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(10.0)),
+              onPressed: () {
 
 
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.only(left: 20.5, right: 20.5),
+            child: RaisedButton(
+              color: Colors.black45,
+              elevation: 10.5,
+              shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(10.0)),
+              onPressed: () {},
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget getFontSizeSlider() {
+    return Container(
+      child: Text('SLider'),
+    );
+  }
+
+  Widget getMarginsSetter() {
+    return Container(
+      child: Text('Set Margin'),
+    );
+  }
+
+  Widget getLineHeightSetter() {
+    return Container(
+      child: Text('set Line Height'),
+    );
+  }
+
+  Widget getFontSetter() {
+    return Container(
+      child: Text('Set Font'),
     );
   }
 }
