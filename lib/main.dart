6eb28ef9eec4a26c6ead0572df8eb4dfb709476a
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2019-09-23 06:40:26 +0530

    Theme and Layout Widgets ready

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue,
+        accentColor: Colors.cyan,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
